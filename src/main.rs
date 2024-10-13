8973b335c2f0f098f111e69c7e0f4c750cdd2dc8
NebPL <160783369+NebPL@users.noreply.github.com>
2024-10-13 22:14:43 +0200

    Taschenrechner in der Console
    Das ist ein einfacher Taschenrechner in der Console

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+
+
+    println!("Willkommen zum der Taschenrechner app!");
+
+    println!("Gib deine erste Zahl ein:");
+    let mut input1 = String::new();
+        io::stdin()
+            .read_line(&mut input1)
+            .expect("Fehler beim Lesen der Eingabe");
+
+    let input1: f64 = input1.trim().parse().expect("Bitte gib eine gültige Zahl ein");
+    println!("Gib den operator ein:");
+    let mut operator = String::new();
+    io::stdin()
+        .read_line(&mut operator)
+        .expect("Fehler beim Lesen der Eingabe");
+
+
+    println!("Gib deine zweite Zahl ein:");
+    let mut input2 = String::new();
+    io::stdin()
+        .read_line(&mut input2)
+        .expect("Fehler beim Lesen der Eingabe");
+    let input2: f64 = input2.trim().parse().expect("Bitte gib eine gültige Zahl ein");
+
+    match operator.as_str().trim() {
+        "+" => { println!("{}",input1+input2)}
+        "-" => { println!("{}",input1-input2)}
+        "*" => { println!("{}",input1*input2)}
+        "/" => { println!("{}",input1/input2)}
+        _ =>{}
+    }
+
+
 }
